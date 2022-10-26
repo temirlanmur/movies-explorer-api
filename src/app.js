d43685f66b83a0f77550149fed0dbf2cef38ac65
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2022-10-26 18:49:07 +0600

    feat: add rate limit and helmet

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const helmet = require('helmet');
 const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger/swagger.json');
 const { MONGO_CONNECTION_STRING, IS_DEVELOPMENT } = require('./environment');
 const { requestLogger, errorLogger } = require('./middleware/logger');
+const { limiter } = require('./middleware/limiter');
 const { cors } = require('./middleware/cors');
 const { errorHandler } = require('./middleware/errorHandler');
 const { useMainRouter } = require('./routes');
@@ -13,7 +15,11 @@ mongoose.connect(MONGO_CONNECTION_STRING);
 
 const app = express();
 
+app.use(helmet());
+
 app.use(requestLogger);
+
+app.use(limiter);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
